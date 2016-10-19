54b05d8a2210a6b28d71b5035a55c1a5a8e99d93
jignesh.rawal <jignesh.rawal@bitbucket.aon.com>
2016-10-19 16:01:35 +0530

    Sample Node-ES6 server setup using Babel
    Sample Node-ES6 server setup using Babel

---
+++ Node-ES6-using-Babel/build/main.js
@@ -0,0 +1,74 @@
+'use strict';
+
+require('./module1');
+
+var _http = require('http');
+
+var _http2 = _interopRequireDefault(_http);
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_http2.default.createServer(function (request, response) {
+    console.log('request starting... ', request.url);
+
+    var filePath = '.' + request.url;
+    if (filePath == './') filePath = './index.html';
+
+    var extname = _path2.default.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.ts':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    _fs2.default.readFile(filePath, function (error, content) {
+        if (error) {
+            if (error.code == 'ENOENT') {
+                _fs2.default.readFile('./404.html', function (error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            } else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+                response.end();
+            }
+        } else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+}).listen(3000);
+
+// NODE SERVER CODE
+//==================================
+
+
+console.log('Server running at http://127.0.0.1:3000/');
